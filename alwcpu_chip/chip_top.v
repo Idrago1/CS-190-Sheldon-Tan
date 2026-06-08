a969e0d4fdbebf505bb94888dfd90819b1e1a46c
Ivan Nguyen <ivannguyen32@gmail.com>
2026-06-08 01:02:37 -0700

    Update ALWCPU chip integration and OpenROAD configuration

--- alwcpu_chip/chip_top.v
+++ alwcpu_chip/chip_top.v
@@ -1,27 +1,22 @@
 module alwcpu_chip(
 
-    // Pads
     input clk_pad,
     input rst_pad,
 
-    // Test interface
     input scan_en,
     input scan_in,
     output scan_out,
 
-    // Debug pads
+    output [7:0] gpio_out,
+
     output [7:0] debug_addr,
     output [7:0] debug_data
 
 );
 
-wire clk;
-wire rst;
+wire clk = clk_pad;
+wire rst = rst_pad;
 
-assign clk = clk_pad;
-assign rst = rst_pad;
-
-// Device interface
 wire [7:0] dvc_wr_addr;
 wire [7:0] dvc_rd_addr;
 wire [7:0] data_mem2dvc;
@@ -30,15 +25,93 @@ wire [7:0] data_dvc2mem;
 wire dvc_wr;
 wire dvc_rd;
 
-// Simple test stub
-assign scan_out = scan_in;
-
-// Debug pads
 assign debug_addr = dvc_wr_addr;
 assign debug_data = data_mem2dvc;
 
-// No external device attached
-assign data_dvc2mem = 8'h00;
+assign scan_out = scan_in;
+
+////////////////////////////////////////////////
+// GPIO Peripheral
+////////////////////////////////////////////////
+
+reg [7:0] gpio_reg;
+
+always @(posedge clk)
+begin
+    if (rst)
+        gpio_reg <= 8'h00;
+    else if (dvc_wr && dvc_wr_addr == 8'h10)
+        gpio_reg <= data_mem2dvc;
+end
+
+assign gpio_out = gpio_reg;
+
+////////////////////////////////////////////////
+// Timer Peripheral
+////////////////////////////////////////////////
+
+reg [31:0] timer_count;
+
+always @(posedge clk)
+begin
+    if (rst)
+        timer_count <= 32'd0;
+    else
+        timer_count <= timer_count + 1'b1;
+end
+
+////////////////////////////////////////////////
+// Extra Registers (increases logic)
+////////////////////////////////////////////////
+
+reg [7:0] reg0;
+reg [7:0] reg1;
+reg [7:0] reg2;
+reg [7:0] reg3;
+
+always @(posedge clk)
+begin
+    if (rst)
+    begin
+        reg0 <= 0;
+        reg1 <= 0;
+        reg2 <= 0;
+        reg3 <= 0;
+    end
+    else if (dvc_wr)
+    begin
+        case (dvc_wr_addr)
+            8'h30: reg0 <= data_mem2dvc;
+            8'h31: reg1 <= data_mem2dvc;
+            8'h32: reg2 <= data_mem2dvc;
+            8'h33: reg3 <= data_mem2dvc;
+        endcase
+    end
+end
+
+////////////////////////////////////////////////
+// SRAM Interface
+////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////
+// Read Mux
+////////////////////////////////////////////////
+
+assign data_dvc2mem =
+        (dvc_rd_addr == 8'h10) ? gpio_reg :
+        (dvc_rd_addr == 8'h20) ? timer_count[7:0] :
+        (dvc_rd_addr == 8'h30) ? reg0 :
+        (dvc_rd_addr == 8'h31) ? reg1 :
+        (dvc_rd_addr == 8'h32) ? reg2 :
+        (dvc_rd_addr == 8'h33) ? reg3 :
+
+        8'h00;
+
+////////////////////////////////////////////////
+// CPU
+////////////////////////////////////////////////
 
 ClaiRISC_core cpu (
     .clk(clk),
--- alwcpu_chip/config.mk
+++ alwcpu_chip/config.mk
@@ -18,16 +18,42 @@ $(DESIGN_HOME)/$(PLATFORM)/alwcpu_chip/rom128x12.v \
 $(DESIGN_HOME)/$(PLATFORM)/alwcpu_chip/rom256x12.v \
 $(DESIGN_HOME)/$(PLATFORM)/alwcpu_chip/rom512x12.v \
 $(DESIGN_HOME)/$(PLATFORM)/alwcpu_chip/rom1024x12.v \
-$(DESIGN_HOME)/$(PLATFORM)/alwcpu_chip/rom2048x12.v
+$(DESIGN_HOME)/$(PLATFORM)/alwcpu_chip/rom2048x12.v \
+
+
 
 export SDC_FILE = \
 $(DESIGN_HOME)/$(PLATFORM)/alwcpu_chip/constraint.sdc
 
+
+
+
 export DIE_AREA = 0 0 1500 1500
 export CORE_AREA = 100 100 1400 1400
 
+export GPL_TIMING_DRIVEN = 0
 export PLACE_DENSITY = 0.30
+export GPL_ROUTABILITY_DRIVEN = 0
 
 export FLOW_VARIANT = base
 
 export SYNTH_MEMORY_MAX_BITS = 50000
+
+export GPL_TIMING_DRIVEN = 0
+export GPL_ROUTABILITY_DRIVEN = 0
+
+export SKIP_INCREMENTAL_REPAIR = 1
+export SKIP_GATE_CLONING = 1
+export SKIP_RESIZER = 1
+
+
+export SKIP_INCREMENTAL_REPAIR = 1
+export SKIP_RESIZER = 1
+export SKIP_CTS_REPAIR_TIMING = 1
+
+
+export SKIP_RESIZER = 1
+export SKIP_RESIZER_DESIGN = 1
+export GPL_TIMING_DRIVEN = 0
+export GPL_ROUTABILITY_DRIVEN = 0
+export SKIP_RESIZER_DESIGN = 1
